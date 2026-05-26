d0a9ad63c4c7828a03ab0570b54a162db8397b33
Angie0945 <ajvargasp@eafit.edu.co>
2026-05-26 16:20:50 -0500

    Update Botones.py

--- pages/Botones.py
+++ pages/Botones.py
@@ -47,7 +47,7 @@ else:
     st.write('')
 
 if st.button('APAGAR'):
-    act1="APAGAR LA ALARMA"
+    act1="APAGAR ALARMA"
     client1= paho.Client("GIT-HUBA")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
